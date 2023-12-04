83c9e15c6fe7e5fba1f2701a95361b43ee6f619d
Lucas Rocha <lucas@Air-de-Lucas.domain.name>
2023-12-03 22:19:02 -0300

    estrutura inicial

---
+++ src/test/java/br/com/fiap/streaming/StreamingVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
